9097f8f26de84f16ade529f572f1bfbb2633da0c
consr2 <qqq4861@naver.com>
2022-12-28 22:36:38 +0900

    junit 테스트 사용

--- shop/src/test/java/com/example/demo/ShopApplicationTests.java
+++ shop/src/test/java/com/example/demo/ShopApplicationTests.java
@@ -1,33 +1,82 @@
 package com.example.demo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import com.example.demo.book.BookService;
 import com.example.demo.order.OrderService;
+import com.example.demo.user.UserControllerAPI;
+import com.example.demo.user.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.RequiredArgsConstructor;
+
+@AutoConfigureMockMvc
+@TestConstructor(autowireMode = AutowireMode.ALL)
+@RequiredArgsConstructor
 @SpringBootTest
 class ShopApplicationTests {
 
-	@Autowired
-	private BookService bookService;
-	@Autowired
-	private OrderService orderService;
-	
-	
+	private final BookService bookService;
+	private final OrderService orderService;
+	private final UserControllerAPI userControllerApi;
+	private final MockMvc mockMvc;
 
-	//책등록
-	//@Test
-	void createBook() {
-		bookService.create("삼국지", 10000, 100);
-		bookService.create("초한지", 15000, 150);
+	//유저 등록 테스트
+	@Test
+	void createBook() throws Exception{
+		
+		UserDto user = new UserDto("호랑이", 15);
+		String json = new ObjectMapper().writeValueAsString(user);
+		
+		mockMvc.perform(post("/api/v2/user")
+				.content(json)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andDo(print());
 	}
+	
+	//유저 목록조회 테스트
+	//@Test
+	public void getTest() throws Exception{
+		mockMvc.perform(get("/api/v2/user/select")
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$data").exists())
+		.andDo(print());
+	}
+	
+	
 	
 	//주문 등록
-	//@Test
-	void createOrder() {
-		orderService.create("홍길동", "삼국지", 20000, 2);
-		orderService.create("홍길동", "초한지", 60000, 3);
-	}
+		//@Test
+		void createOrder() {
+			orderService.create("홍길동", "삼국지", 20000, 2);
+			orderService.create("홍길동", "초한지", 60000, 3);
+		}
+	
+	
+	
+	
+	
+	
+	
+	
 }
